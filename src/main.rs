7ab4385142f572ac1541fa4da4f21dd41539eca7
Albertbuh <albertbuhovets@mail.ru>
2024-10-03 13:24:33 +0300

    3 led flashs after every 2 clicks

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use panic_halt as _;
 
 use nb::block;
 
-use core::cell::{Cell, RefCell};
+use core::{borrow::BorrowMut, cell::{Cell, RefCell}};
 use cortex_m::interrupt::Mutex;
 use cortex_m_rt::entry;
 use stm32f1xx_hal::{
@@ -19,8 +19,12 @@ type LedPin = gpio::PA5<Output>;
 static G_BUTTON: Mutex<RefCell<Option<ButtonPin>>> = Mutex::new(RefCell::new(None));
 static G_TIM: Mutex<RefCell<Option<CounterMs<TIM2>>>> = Mutex::new(RefCell::new(None));
 static G_LED: Mutex<RefCell<Option<LedPin>>> = Mutex::new(RefCell::new(None));
-static G_DELAYMS: Mutex<Cell<u32>> = Mutex::new(Cell::new(2000_u32));
 
+static CLICK_COUNT: Mutex<Cell<u32>> = Mutex::new(Cell::new(0_u32));
+static IS_SHINING: Mutex<Cell<bool>> = Mutex::new(Cell::new(false));
+static SHINE_COUNT: Mutex<Cell<u32>> = Mutex::new(Cell::new(0_u32));
+const LED_TOGGLE_TO_STOP: u32= 6;
+const TIM_DELAYMS: u32 = 500;
 #[entry]
 fn main() -> ! {
     let mut dp = pac::Peripherals::take().unwrap();
@@ -44,10 +48,10 @@ fn main() -> ! {
     let mut afio = dp.AFIO.constrain(); 
     button.make_interrupt_source(&mut afio);
     //     External interrupt/event controller
-    button.trigger_on_edge(&mut dp.EXTI, Edge::Rising);
+    button.trigger_on_edge(&mut dp.EXTI, Edge::Falling);
     button.enable_interrupt(&mut dp.EXTI);
 
-    timer.start(2000.millis()).unwrap();
+    timer.start(TIM_DELAYMS.millis()).unwrap();
     timer.listen(Event::Update);
     //  3. NVIC - interruption controller, we need to tell him which interruption we want to use
     //     and we need to unmask it
@@ -76,26 +80,23 @@ fn EXTI15_10() {
 
     // Start a Critical Section
     cortex_m::interrupt::free(|cs| {
-        // Obtain Access to Delay Global Data and Adjust Delay
-        G_DELAYMS
-            .borrow(cs)
-            .set(G_DELAYMS.borrow(cs).get() - 500_u32);
+        let click_count = CLICK_COUNT.borrow(cs);
+        click_count.set(click_count.get() + 1);
 
-        // Reset delay value if it drops below 500 milliseconds
-        if G_DELAYMS.borrow(cs).get() < 500_u32 {
-            G_DELAYMS.borrow(cs).set(2000_u32);
+        if click_count.get() % 2 == 0 {
+            IS_SHINING.borrow(cs).set(true);
+             // Obtain access to global timer
+            let mut timer = G_TIM.borrow(cs).borrow_mut();
+
+            // Adjust and start timer with updated delay value
+            timer
+                .as_mut()
+                .unwrap()
+                .start(TIM_DELAYMS.millis())
+                .unwrap();
         }
 
-        // Obtain access to global timer
-        let mut timer = G_TIM.borrow(cs).borrow_mut();
-
-        // Adjust and start timer with updated delay value
-        timer
-            .as_mut()
-            .unwrap()
-            .start(G_DELAYMS.borrow(cs).get().millis())
-            .unwrap();
-
+       
         // Obtain access to Global Button Peripheral and Clear Interrupt Pending Flag
         let mut button = G_BUTTON.borrow(cs).borrow_mut();
         button.as_mut().unwrap().clear_interrupt_pending_bit();
@@ -105,9 +106,19 @@ fn EXTI15_10() {
 #[interrupt]
 fn TIM2() {
     cortex_m::interrupt::free(|cs| {
-        let mut led = G_LED.borrow(cs).borrow_mut();
-        led.as_mut().unwrap().toggle();
+        if IS_SHINING.borrow(cs).get() {
+            let mut led = G_LED.borrow(cs).borrow_mut();
+            led.as_mut().unwrap().toggle();
 
+            let shine_count = SHINE_COUNT.borrow(cs);
+            shine_count.set(shine_count.get() + 1);
+
+            if shine_count.get() >= LED_TOGGLE_TO_STOP {
+                IS_SHINING.borrow(cs).set(false);
+                shine_count.set(0);
+            }
+        }
+        
         let mut timer = G_TIM.borrow(cs).borrow_mut();
         timer.as_mut().unwrap().clear_interrupt(Event::Update);
     });
